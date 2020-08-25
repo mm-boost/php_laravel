78c0d7cef58cb27e92db88117e8ee4b49628bdff
Mutou manami <yori949dmq@kir.biglobe.ne.jp>
2020-08-25 13:22:51 +0000

    php.newprofilec

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //課題問題５
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+}
