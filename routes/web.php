d65c6b19d22773344114f58f57be3ce16c83037f
Mutou manami <yori949dmq@kir.biglobe.ne.jp>
2020-08-26 13:09:43 +0000

    Routing設定

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create','ProfileController@add');
+    Route::get('profile/edit','ProfileController@edit');
+});
+
+//PHP/Laravel 09 Routingについて理解する
+/*課題
+１.URLとControllerやActionを紐付ける機能を何といいますか？
+ Routing  Webアプリケーションで設定したURLにアクセスすると、URLに応じたcontrollerやActionを呼び出せるように設定する機能である
+ 
+2.あなたが考える、group化をすることのメリットを考えてみてください。
+ グループ化することで、一度に複数のcontrollerにURLとのrouting設定が行えるから。
+ 
+３.「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
+Route::get('xxx','Admin\AAAController@bbb');
+
+4.【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、
+           admin/profile/create にアクセスしたら ProfileController の add Action に、
+           admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。 
+上記コードにて追加作成*/
+
+
