9f1318525b54d9ab6e252ae06bf8dee1c3cf5e22
Mutou manami <yori949dmq@kir.biglobe.ne.jp>
2020-08-26 13:17:55 +0000

    Routingの設定

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/create','ProfileController@add');
-    Route::get('profile/edit','ProfileController@edit');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
 });
 
 //PHP/Laravel 09 Routingについて理解する
