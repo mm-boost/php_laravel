17ae6e6e0035ef7f750ea10b400b63733c3c4660
Mutou manami <yori949dmq@kir.biglobe.ne.jp>
2020-08-27 12:20:22 +0000

    controllerとviewの連携

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 class ProfileController extends Controller
 {
     //課題問題５
+    //10章 ControllerとViewが連携 課題問題４
     public function add()
     {
         return view('admin.profile.create');
@@ -18,6 +19,7 @@ class ProfileController extends Controller
         return redirect('admin/profile/create');
     }
 
+    //10章 ControllerとViewが連携 課題問題４
     public function edit()
     {
         return view('admin.profile.edit');
@@ -29,3 +31,14 @@ class ProfileController extends Controller
     }
 }
 
+/*課題
+1 Viewは何をするところでしょうか。簡潔に説明してみてください。
+ controllerの指示で、アクセスしたきたユーザーのブラウザに表示するデータを生成する場所
+
+2 プログラマーがHTMLを書かずにPHPなどのプログラミング言語やフレームワークを使う必要があるのはどういった理由でしょうか。
+　Webpページの表示を、ログインしてきたユーザーに合わせた表示ができるようにするため。
+　その設定を行うには、Mobile経由でデータベースのデータを取得し、htmlファイルにプログラミング言語やフレームワークを使用して
+　記述する必要があるため。
+　
+3,4 前々章でAdmin/ProfileControllerのadd Action, edit Action に記述したコードに
+　　それぞれどこのディレクトリに何というbladeファイルを設置すれば良いか考え、実際に作成する。
