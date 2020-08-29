cb5db6b34c7ff54e29e0c96bdda73024578449df
Mutou manami <yori949dmq@kir.biglobe.ne.jp>
2020-08-29 08:50:15 +0000

    Bladeテンプレートの継承とLaravel Mix

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,3 +11,22 @@
         <h1>プロフィール作成画面</h1>
     </body>
 </html>
+
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
