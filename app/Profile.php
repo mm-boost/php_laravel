6757e6c7e669c8108ac324f9b0f574b307fc7c5c
Mutou manami <yori949dmq@kir.biglobe.ne.jp>
2020-09-04 03:30:12 +0000

    Model機能搭載

---
+++ app/Profile.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    //以下、名前(name)、性別(gender)、趣味(hobby)、自己紹介(introduction)のValidation設定を行う
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+}
